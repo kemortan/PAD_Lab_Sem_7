de8251a86779da5087277c122a482b8e1d2e6181
Moscalu Constantin <constantin.moscalu@ati.utm.md>
2020-09-23 22:26:03 +0300

    Lab1

---
+++ Agent Mesagerie/Broker/Program.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+using System;
+
+namespace Broker
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               Console.WriteLine("Broker.");
+
+               BrokerSocket socket = new BrokerSocket();
+               socket.Start(Settings.BROKER_IP, Settings.BROKER_PORT);
+
+               Console.ReadLine();
+          }
+     }
+}
